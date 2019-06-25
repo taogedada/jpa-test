dc3356b6a7f00a41b96cec814a363f7e55d5eee1
taogedada <49275730+taogedada@users.noreply.github.com>
2019-06-25 10:57:37 +0800

    Update MyApplication.java

--- test1/src/main/java/MyApplication.java
+++ test1/src/main/java/MyApplication.java
@@ -1,10 +1,10 @@
-import org.springframework.boot.SpringApplication;
+1import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication(scanBasePackages = "cn.rivamed.*")
 public class MyApplication {
-    public static void main(String[] args) {String a = "13";
+    public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
     }
 }
